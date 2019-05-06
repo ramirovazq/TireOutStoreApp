2e7991e980bb4a5807449bfb20f3490ea19b7247
ramirovaz <r.vazquez@meditecsols.com>
2019-05-06 08:08:11 -0500

    using currentUsername

--- App.js
+++ App.js
@@ -10,20 +10,21 @@ export default class App extends React.Component {
     this.state = {
       currentUser: null,
       tokenUser: null,
+      currentUsername: null
     };
 
     this.asignUserCallback = this.asignUserCallback.bind(this);
 
   } //constructor
 
-   asignUserCallback(tokenFromChild, userFromChild) {
-        this.setState({ tokenUser: tokenFromChild, currentUser: userFromChild });
+   asignUserCallback(tokenFromChild, userFromChild, usernameFromChild) {
+        this.setState({ tokenUser: tokenFromChild, currentUser: userFromChild, currentUsername: usernameFromChild });
    }
 
   render() {
     	if (!this.state.tokenUser) {
       		return <LoginAlmacen asignUser={this.asignUserCallback}></LoginAlmacen>
 		}
-		return <Home tokenUser={this.state.tokenUser}  currentUser={this.state.currentUser} />;
+		return <Home tokenUser={this.state.tokenUser}  currentUser={this.state.currentUser} currentUsername={this.state.currentUsername} asignUser={this.asignUserCallback} />;
   }
 }
