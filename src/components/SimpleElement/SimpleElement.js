3235cc16da4ba291dd6680c041a2058ab2bd9939
ramirovaz <r.vazquez@meditecsols.com>
2019-05-07 20:05:04 -0500

    draws a scrol, with flatlist

--- src/components/SimpleElement/SimpleElement.js
+++ src/components/SimpleElement/SimpleElement.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, FlatList, Text, View } from 'react-native';
+import SubListElement from '../SubListElement/SubListElement';
 
 export default class SimpleElement extends Component {
 
   render() {
     return (
-      <View><Text>{this.props.llanta.dot}</Text></View>
+      <View>
+      	<Text>{this.props.llanta.dot} {this.props.llanta.marca.nombre}</Text>
+      	<FlatList
+          data={[
+            {key: 'Devin'},
+            {key: 'Julie'},
+          ]}
+          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
---
+++ src/components/SubListElement/SubListElement.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class SubListElement extends Component {
+
+  render() {
+    return (
+        <TouchableOpacity>
+          <Text> Sublista </Text>
+        </TouchableOpacity>
+    );
+  }
+}
+
