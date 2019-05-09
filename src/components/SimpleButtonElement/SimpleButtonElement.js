3e64ab07bc09ea7516a967072adfcb01e9c08bf6
ramirovaz <r.vazquez@meditecsols.com>
2019-05-09 06:44:56 -0500

    button stable version

--- src/components/SimpleButtonElement/SimpleButtonElement.js
+++ src/components/SimpleButtonElement/SimpleButtonElement.js
@@ -7,8 +7,7 @@ export default class SimpleButtonElement extends Component {
   constructor(props) {
       super(props)
       this.state = { 
-        count: 0,
-        enabledbutton: null
+        count: 0
       }
       this.checkLimits = this.checkLimits.bind(this);
   }
@@ -19,39 +18,27 @@ export default class SimpleButtonElement extends Component {
     })
   }
 
-  checkLimits(minnumber=0, maxnumber=4) {
-    let check = (this.state.count < maxnumber) && (this.state.count > minnumber);
-    console.log("--------------check");
-    console.log(check);
-    console.log("--------------enabledbutton");
-    console.log(this.state.enabledbutton);
-    return check
-  }
-
-
-
   lessCount = () => {
     this.setState({
       count: this.state.count - 1
     })
-    if (this.state.count === this.props.minNumber) {
-      this.setState({
-        enabledbutton: false
-      })
-    } else {
-      this.setState({
-        enabledbutton: true
-      })      
-    }
   }
 
+  checkLimits(minnumber=0, maxnumber=4) {
+    let check = (this.state.count < maxnumber) && (this.state.count >= minnumber);
+    return check
+  }
   
   render() {
     const renderAction = this.props.isRemoval;
     const maxnumberProps = this.props.maxNumber;
     const minnumberProps = this.props.minNumber;
     return (
-          <TouchableOpacity style={this.state.enabledbutton ? styles.button : styles.buttondis } onPress={this.lessCount} disabled={!this.checkLimits(minnumberProps, maxnumberProps)} >
+          <TouchableOpacity 
+            style={this.checkLimits(minnumberProps, maxnumberProps) ? styles.button : styles.buttondis } 
+            onPress={this.addCount} 
+            disabled={!this.checkLimits(minnumberProps, maxnumberProps)} 
+          >
           <Text style={styles.buttonText}> + { this.state.count !== 0 ? this.state.count: null } </Text>
           </TouchableOpacity>
     );
@@ -62,8 +49,8 @@ const styles = StyleSheet.create({
     button: {
     alignItems: 'center',
     backgroundColor: 'powderblue',
-    width: 30,
-    height: 20,
+    width: 40,
+    height: 25,
   },
   buttonText:{
     fontSize: 15,
@@ -73,7 +60,7 @@ const styles = StyleSheet.create({
   buttondis: {
     alignItems: 'center',
     backgroundColor: '#cbcbcb',
-    width: 30,
-    height: 20,
+    width: 40,
+    height: 25,
   },
 });
